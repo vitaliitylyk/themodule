14bcb404f32ccd5691e6f3d711387ae698fef9df
Vitalii Tylyk <vitaliytyl@gmail.com>
2024-09-22 16:34:56 +0200

    Make it generic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vitaliitylyk/themodule
 
 go 1.22.6
+
+require golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
--- themodule.go
+++ themodule.go
@@ -1,6 +1,12 @@
 package themodule
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of two integers.
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
